59103323197aa15559954d3d5561e14f89e3328d
Basjabbie <100725442+Basjabbie@users.noreply.github.com>
2024-03-11 13:32:00 +0000

    1

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,16 +1,16 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', { value: true });
-const tslib_1 = require('tslib');
-const express_1 = tslib_1.__importDefault(require('express'));
-const weatherRoute_js_1 = tslib_1.__importDefault(
-  require('./routes/weatherRoute.js'),
-);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tslib_1 = require("tslib");
+const express_1 = tslib_1.__importDefault(require("express"));
+const weatherRoute_js_1 = tslib_1.__importDefault(require("./routes/weatherRoute.js"));
+const cors_1 = tslib_1.__importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 //our new route
 app.use('/api/weather', weatherRoute_js_1.default);
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
 //# sourceMappingURL=server.js.map
